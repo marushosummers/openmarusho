b23c72b137e3f7d5bc83151ba04ddd02090e9e5a
marusho <mrsho34@gmail.com>
2021-05-25 15:30:29 +0900

    delete: configure in main branch

--- dashboard/src/plugins/firebase.js
+++ dashboard/src/plugins/firebase.js
@@ -5,13 +5,7 @@ import "firebase/functions";
 import "firebase/analytics";
 
 const config = {
-  apiKey: "AIzaSyCA2Gi83b2KVKcrf5GCdQ4ZIomUSJMYsI8",
-  authDomain: "open-marusho.firebaseapp.com",
-  projectId: "open-marusho",
-  storageBucket: "open-marusho.appspot.com",
-  messagingSenderId: "76478453915",
-  appId: "1:76478453915:web:78906e53f4b4d9069c8e3e",
-  measurementId: "G-WNTBVPMBPE",
+  // Input Your Config.
 };
 
 firebase.initializeApp(config);
